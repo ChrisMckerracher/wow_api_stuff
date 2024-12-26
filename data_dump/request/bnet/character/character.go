99dbab698884d911fe75278f0655eb896ccf994f
Christopher Mckerracher <chrislmckerracher@gmail.com>
2024-12-26 00:26:32 -0500

    update: :)

--- data_dump/request/bnet/character/character.go
+++ data_dump/request/bnet/character/character.go
@@ -4,10 +4,26 @@ import (
 	"data_dump/request/bnet"
 )
 
+// todo: move these
+type Character string
+
+// ToDo: typify realms, character and realm types are cross api also :)
+type Server string
+
 type Client struct {
 	bnet.RegionalClient
 }
 
 func (c *Client) Scrape() {
 
+}
+
+func (c *Client) ScrapeUser(user Character, realm Server, region bnet.Region) {
+	bnetClient, err := c.Select(region)
+
+	if err != nil {
+
+	}
+
+	//bnetClient.WoWCharacterMythicKeystoneProfile(c.Context, string(realm), string(user))
 }
--- data_dump/request/bnet/client.go
+++ data_dump/request/bnet/client.go
@@ -2,13 +2,34 @@ package bnet
 
 import (
 	"context"
+	"fmt"
 	"github.com/FuzzyStatic/blizzard/v3"
 )
 
-// ToDo rename
 type RegionalClient struct {
-	context  context.Context
+	Context  context.Context
 	usClient blizzard.Client
 	euClient blizzard.Client
 	twClient blizzard.Client
+}
+
+type Region int
+
+const (
+	US Region = iota
+	EU
+	TW
+)
+
+func (r *RegionalClient) Select(region Region) (*blizzard.Client, error) {
+	switch region {
+	case US:
+		return &r.usClient, nil
+	case EU:
+		return &r.euClient, nil
+	case TW:
+		return &r.twClient, nil
+	default:
+		return nil, fmt.Errorf("Invalid Region")
+	}
 }
